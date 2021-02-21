c09c93c08c6a4854f88499133cb4f4dc3f7d3dcc
Jeff <lagosjf@gmail.com>
2021-02-21 21:12:31 +0100

    first

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from '../images/inte1.jpg';
+import ImageTwo from '../images/inte2.jpg';
+
+ const InfoData = {
+    heading: 'hola como esta amigo bien?',
+    paragraphOne: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+    paragraphTwo: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+    buttonLabel: 'view homes',
+    image: ImageOne,
+    reverse: 'false',
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern design',
+    paragraphOne: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+    paragraphTwo: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+    buttonLabel: 'view homes',
+    image: ImageTwo,
+    reverse: 'true',
+    delay: 300
+};
+
+export default InfoData; 
---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from '../images/1.jpg'
+import ImageTwo from '../images/2.jpg'
+import ImageThree from '../images/3.jpg'
+import ImageFour from '../images/4.jpg'
+
+export const SliderData = [
+    {
+        title: 'Luxury villa in Bali, Indonesia',
+        price: '4,000,000$',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House' 
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '2,000,000$',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House' 
+    },
+    {
+        title: 'House in Calabase, California',
+        price: '7,000,000$',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House' 
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '4,000,000$',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House' 
+    },
+]
